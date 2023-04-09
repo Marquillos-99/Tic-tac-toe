2be2c11edcad2d43a788455d6db17a5d88bd72f4
Marckgit <marckgit@gmail.com>
2023-04-09 13:24:43 -0600

    boarda and clicks

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,14 @@ const turns = {
   O: 'o',
 }
 
-const Square = ({ children, updateBoard, index}) => {
+const Square = ({ children, isSelected, updateBoard, index}) => {
+  const className = `square ${isSelected ? 'is-selected': ''}`
+  const handleClick = () => {
+    updateBoard(index)
+  }
+
   return(
-    <div className="square">
+    <div onClick={handleClick} className={className}>
       {children}
     </div>
   )
@@ -15,10 +20,27 @@ const Square = ({ children, updateBoard, index}) => {
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null))
-  const handleBoard = () => {
-    setBoard
-  }
 
+  const [turn, setTurn] = useState(turns.X)
+  
+  // null = no hay ganador false = draw
+  const [winner, setWinner] = useState(null)
+
+  const updateBoard = (index) => {
+    // no se actualiza si tiene algo
+    if(board[index]) return
+
+    // re-render with simbols
+    //important props unmutable
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+
+    // change turn
+    const newTurn = turn === turns.X ? turns.O : turns.X
+    setTurn(newTurn)
+  }
+ 
   return (
     <main className="board">
       <h1>Tic-tac-toe</h1>
@@ -29,12 +51,17 @@ function App() {
               <Square
                 key={index}
                 index = {index}
+                updateBoard={updateBoard}
               >
                 {board[index]}
               </Square>
             )
           })
         }
+      </section>
+      <section className="turn">
+        <Square isSelected={turn === turns.X}>{turns.X}</Square>
+        <Square isSelected={turn === turns.O}>{turns.O}</Square>
       </section>
     </main>
   )
