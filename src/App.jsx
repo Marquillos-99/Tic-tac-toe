5308da4144179c25e385d973e1a25d5a3fcc622b
Marckgit <marckgit@gmail.com>
2023-04-09 13:52:56 -0600

    'setWinner'

--- src/App.jsx
+++ src/App.jsx
@@ -18,17 +18,45 @@ const Square = ({ children, isSelected, updateBoard, index}) => {
   )
 }
 
+const WinnerCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  //verticals
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  //diagonal
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null))
 
   const [turn, setTurn] = useState(turns.X)
-  
+
   // null = no hay ganador false = draw
   const [winner, setWinner] = useState(null)
 
+  const checkWinner = (boardCheck) => {
+    for (const combo of WinnerCombos){
+      const [a, b, c] = combo
+      if (
+        boardCheck[a]&&
+        boardCheck[a] === boardCheck[b] &&
+        boardCheck[a] === boardCheck[c]
+      ){
+        return boardCheck
+      }
+    } 
+    // no Winner
+    return null
+  }
+
   const updateBoard = (index) => {
     // no se actualiza si tiene algo
-    if(board[index]) return
+    if(board[index] || winner) return
 
     // re-render with simbols
     //important props unmutable
@@ -39,6 +67,11 @@ function App() {
     // change turn
     const newTurn = turn === turns.X ? turns.O : turns.X
     setTurn(newTurn)
+    //check if there is a winner
+    const newWinner = checkWinner(newBoard)
+    if(newWinner){
+      setWinner(newWinner)
+    }
   }
  
   return (
